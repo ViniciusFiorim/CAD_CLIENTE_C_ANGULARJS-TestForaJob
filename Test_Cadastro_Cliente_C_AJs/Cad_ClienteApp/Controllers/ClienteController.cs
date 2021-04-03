53b7481115f4934ef0f7c2d2081a9f08afe7208e
ViniciusFiorim <81820659+ViniciusFiorim@users.noreply.github.com>
2021-04-03 11:56:58 -0300

    Add files via upload

---
+++ Test_Cadastro_Cliente_C_AJs/Cad_ClienteApp/Controllers/ClienteController.cs
@@ -0,0 +1,99 @@
+﻿using Cad_ClienteApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cad_ClienteApp.Controllers
+{
+    public class ClienteController : Controller
+    {
+        #region Método para listar - READ
+        // GET Cliente/GetCliente
+        public JsonResult GetCliente() 
+        { 
+            using(var db = new ClientesEntities())
+            {
+                List<Cliente> listarClientes = db.Clientes.ToList();
+
+                return Json(listarClientes, JsonRequestBehavior.AllowGet);
+            }
+        }
+
+        #endregion
+
+        #region Método para adicionar - CREATE
+        //POST Cliente/AddCliente
+        [HttpPost]
+        public JsonResult AddCliente(Cliente cliente) 
+        {
+            if (cliente != null)
+            {
+                using(var db = new ClientesEntities())
+                {
+                    db.Clientes.Add(cliente);
+                    db.SaveChanges();
+
+                    return Json(new { success = true });
+                }
+            }
+            return Json(new { success = false });
+        }
+
+        #endregion
+
+        #region Método para Atualizar - UPDATE
+        [HttpPost]
+        public JsonResult UpdCliente(Cliente cliente)
+        {
+            using (var db = new ClientesEntities())
+            {
+                var clienteUpd = db.Clientes.Find(cliente.ClienteId);
+                
+                if (clienteUpd == null)
+                {
+                    return Json(new { success = false });
+                }
+
+                else
+                {
+                    clienteUpd.Nome = cliente.Nome;
+                    clienteUpd.CPF = cliente.CPF;
+                    clienteUpd.Telefone = cliente.Telefone;
+                    clienteUpd.Endereco = cliente.Endereco;
+                    clienteUpd.Observacao = cliente.Observacao;
+
+                    db.SaveChanges();
+                    return Json(new { success = true });
+                }
+            }            
+        }
+
+
+        #endregion
+
+        #region Método para Ecluir - DELETE
+
+        [HttpPost]
+        public JsonResult DelCliente(int id)
+        {
+            using(var db = new ClientesEntities())
+            {
+                var cliente = db.Clientes.Find(id);
+                if(cliente == null)
+                {
+                    return Json(new { success = false });
+                }
+
+                db.Clientes.Remove(cliente);
+                db.SaveChanges();
+
+                return Json(new { success = true });
+            }            
+        }
+
+        #endregion
+
+    }
+}
---
+++ Test_Cadastro_Cliente_C_AJs/Cad_ClienteApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Cad_ClienteApp.MvcApplication" Language="C#" %>
